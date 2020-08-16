5e3de197b92621106384e90304a889992a2e085a
Gonzalo Vanni <gonzalovanni@gmail.com>
2020-08-16 16:46:11 -0300

    fix process pay route

--- src/routes/products.js
+++ src/routes/products.js
@@ -6,7 +6,7 @@ const productsController = require('../controller/productsController');
 router.get('/detail/:id', productsController.detail);
 router.get('/search',productsController.search)
 router.get('/checkout',productsController.checkout)
-router.get('/procesar_pago',productsController.processPay)
+router.post('/procesar_pago',productsController.processPay)
 
 
 module.exports = router;
