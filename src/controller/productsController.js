b18344425c277b5097aaf9a9d56b97eac7ea228f
Gonzalo Vanni <gonzalovanni@gmail.com>
2020-08-16 18:55:59 -0300

    add webhook endpoint

--- src/controller/productsController.js
+++ src/controller/productsController.js
@@ -60,5 +60,19 @@ module.exports = {
             res.json(error.message);
             });
 
-            }
+    },
+    webhook(req, res) { 
+        if (req.method === "POST") { 
+          let body = ""; 
+          req.on("data", chunk => {  
+            body += chunk.toString();
+          });
+          req.on("end", () => {  
+            console.log(body, "webhook response"); 
+            res.end("ok");
+          });
+        }
+        return res.status(200); 
+      }
+    
 }
