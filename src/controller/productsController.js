f74b80903c84a43e8dc86220e56c49389b5ab4be
Gonzalo Vanni <gonzalovanni@gmail.com>
2020-08-17 18:25:58 -0300

    Update productsController.js

--- src/controller/productsController.js
+++ src/controller/productsController.js
@@ -44,49 +44,33 @@ module.exports = {
         
     },
     checkout: (req,res)=> res.render('checkout'),
-    processPay: async (req,res)=> {
-        var preference = {
-            items: [
-                {
-                    title: 'Test',
-                    quantity: 1,
-                    currency_id: 'ARS',
-                    unit_price: 10.5
-                }
-            ]
+    processPay: async (req,res)=> {      
+
+       await mercadopago.configurations.setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
+
+        var payment_data = {
+        // id:1,
+        transaction_amount: 165,
+        // name:'producto test',
+        token: req.body.token,
+        // cuantity:1,
+        description: req.body.description,
+        // picture_url:"https://www.mercadopago.com/org-img/MP3/home/logomp3.gif",
+        installments: Number(req.body.installments),
+        payment_method_id: req.body.payment_method_id,
+        external_reference:'gonzalo@digitalhouse.com',
+        payer: {
+            id:'471923173',
+            email: req.body.email
+        },
+        // integrator_id:'dev_24c65fb163bf11ea96500242ac130004',
         };
-        
-        mercadopago.preferences.create(preference).then(function (data) {
-            mercadopago.payment.save(preference).then(function (payment) {
-                res.json(payment)
-        }).catch(function (error) {
-            res.json(error.message);
-          });
-    //    await mercadopago.configurations.setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
-    //     var payment_data = {
-    //     // id:1,
-    //     transaction_amount: 165,
-    //     // name:'producto test',
-    //     token: req.body.token,
-    //     // cuantity:1,
-    //     description: req.body.description,
-    //     // picture_url:"https://www.mercadopago.com/org-img/MP3/home/logomp3.gif",
-    //     installments: Number(req.body.installments),
-    //     payment_method_id: req.body.payment_method_id,
-    //     external_reference:'gonzalo@digitalhouse.com',
-    //     payer: {
-    //         id:'471923173',
-    //         email: req.body.email
-    //     },
-    //     // integrator_id:'dev_24c65fb163bf11ea96500242ac130004',
-    //     };
-
-    //     mercadopago.payment.save(payment_data).then(function (data) {
+        mercadopago.payment.save(payment_data).then(function (data) {
            
-    //         res.json(data);
-    //         }).catch(function (error) {
-    //         res.json(error.message);
+            res.json(data);
+            }).catch(function (error) {
+            res.json(error.message);
             });
 
     },
