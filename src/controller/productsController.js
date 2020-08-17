ac97e027cd292242bd1cc5450e294cc358c2952c
Gonzalo Vanni <gonzalovanni@gmail.com>
2020-08-17 16:33:16 -0300

    Update productsController.js

--- src/controller/productsController.js
+++ src/controller/productsController.js
@@ -1,6 +1,6 @@
 var mercadopago = require('mercadopago');
 mercadopago.configure({
-    access_token: "APP_USR-7eb0138a-189f-4bec-87d1-c0504ead5626"
+    access_token: "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398"
   });
   
 const  axios  = require("axios")
